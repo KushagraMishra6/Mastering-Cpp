42079381a79b563d5225b97d7dd7e12d06b52513
Kushagra Mishra <kmishra0204@gmail.com>
2025-06-19 18:45:21 +0530

    Corrected header file

--- 01_Basics/myheader.h
+++ 01_Basics/myheader.h
@@ -1,7 +1,9 @@
-// this is vctr.h file
+// This is a user defined header file must be included using "" and not the <>
 #include<iostream>
 #include<vector>
 using namespace std;
 
 typedef vector <float> vf;
 typedef vector <vector<float>> vvf;
+
+// Or we can use -> "using vf = vector <float>;"
